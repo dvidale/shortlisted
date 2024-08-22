1bc71a35f0aca4e8c1b41080c1ed4f4dc413356d
DeAndre <deandre.vidale@gmail.com>
2024-08-22 00:10:50 -0700

    updated schema code

--- app/models/user_job_title.py
+++ app/models/user_job_title.py
@@ -2,9 +2,11 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 
 
-user_job_titles = db.Table(add_prefix_for_prod('user_job_titles'),
+user_job_titles = db.Table('user_job_titles',
                            db.Column('user_id', db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), primary_key=True),
                            db.Column('job_title_id', db.Integer, db.ForeignKey(add_prefix_for_prod('job_titles.id')), primary_key=True),
                         
-                            **({'schema': SCHEMA} if environment == "production" else {})
                            )
+
+if environment == "production":
+    user_job_titles.schema = SCHEMA
