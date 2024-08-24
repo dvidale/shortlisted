2e8556cffb79decb65cc8484c3ae8bd3592b6ece
DeAndre <deandre.vidale@gmail.com>
2024-08-24 15:04:36 -0700

    successfully building shortlist record, but need dates

--- app/api/shortlists_routes.py
+++ app/api/shortlists_routes.py
@@ -1,15 +1,16 @@
 from flask import Blueprint, request
-from app.models import Shortlist, db, Job_Title
+from app.models import Shortlist, db, Job_Title, Genre, Industry_Area, Location
 from app.forms.save_shortlist_form import SaveShortlistForm
+import json
 
 shortlists_routes = Blueprint('shortlists', __name__)
 
 @shortlists_routes.route('/new', methods=['POST'])
 def save_shortlists():
 
-    request_data = request.json
+    
 
-    print('request_data in save route:', request_data)
+    
 
 
 # write a route to collect the 
@@ -31,16 +32,48 @@ def save_shortlists():
     title = save_shortlist_form.data['title']
     description = save_shortlist_form.data['description']
 
+    
 
 
     if save_shortlist_form.validate_on_submit():
         
-        job_title_id = db.select(Job_Title.id).where(Job_Title.job_title == '')
+        request_data = request.json
+    # .json turns request data into a dict
 
+        job_title = request_data['job_title']
+        industry_area = request_data['industry_area']
+        genre = request_data['genre']
+        location = request_data['location']
+        created_by = request_data['created_by']
+
+        job_title_id = db.session.scalars(db.select(Job_Title.id).where(Job_Title.job_title == job_title)).first()
+
+        industry_area_id = db.session.scalars(
+            db.select(Industry_Area.id).where(Industry_Area.industry_area == industry_area)
+        ).first()
         
-        newShortlist = Shortlist()
+        genreId = db.session.scalars(db.select(Genre.id).where(Genre.genre_name == genre)).first()
 
-        return {'message': 'end of save shortlist route'}
+        locationId = db.session.scalars( db.select(Location.id).where(Location.city == location)).first()
+
+
+        print(">>> data after queries", title, description, job_title_id, industry_area_id, genreId, locationId,created_by)
+        
+        newShortlist = Shortlist(
+            title= title,
+            description=description,
+            job_title_id=job_title_id,
+            industry_area_id=industry_area_id,
+            genre_id=genreId,
+            location_id=locationId,
+            created_by_id=created_by
+        )
+
+        print(">>> newShortlist", newShortlist.to_dict()) 
+
+        return {'message': 'shortlist saved successfully'}
+    
+    return {'error': save_shortlist_form.errors}
 
 # after the shortlist is created, created a referral record for each user in the shortlist
 
--- app/models/shortlist.py
+++ app/models/shortlist.py
@@ -10,17 +10,35 @@ class Shortlist(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.String(255), nullable=True)
+    description = db.Column(db.String(255), nullable=True, default=None)
     job_title_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('job_titles.id')) )
     industry_area_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('industry_areas.id')) )
-    genre_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('genres.id')) )
+    genre_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('genres.id')), nullable=True, default=None)
     location_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('locations.id')))
-    start_date = db.Column(db.DateTime, nullable = True)
-    end_date = db.Column(db.DateTime, nullable = True)
-    optional_img = db.Column(db.String(255))
+    start_date = db.Column(db.DateTime, nullable = True, default=None)
+    end_date = db.Column(db.DateTime, nullable = True, default=None)
+    optional_img = db.Column(db.String(255), default=None)
     created_by_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')) )
     createdAt = db.Column(db.DateTime, nullable=False, default=db.func.now())
     updatedAt = db.Column(db.DateTime, nullable=False, default=db.func.now(), onupdate=db.func.now())
 
     referrals = db.relationship('Referral', cascade='all, delete-orphan')
+
+    def to_dict(self):
+        return{
+            'id':self.id,
+            'title': self.title,
+            'description': self.description,
+            'job_title_id': self.job_title_id,
+            'industry_area_id': self.industry_area_id,
+            'genre_id': self.genre_id,
+            'location_id':self.location_id,
+            'start_date': self.start_date,
+            'end_date': self.end_date,
+            'optional_img': self.optional_img,
+            'created_by_id': self.created_by_id,
+            'createdAt':self.createdAt,
+            'updatedAt': self.updatedAt
+        }
+        
     
