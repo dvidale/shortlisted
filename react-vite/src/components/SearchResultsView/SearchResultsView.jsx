a7cb592456f2128f7e7e16dab7fe16d6a3afe138
DeAndre <deandre.vidale@gmail.com>
2024-08-27 11:16:42 -0700

    frontend availability filtering now working

--- react-vite/src/components/SearchResultsView/SearchResultsView.jsx
+++ react-vite/src/components/SearchResultsView/SearchResultsView.jsx
@@ -18,8 +18,45 @@ function SearchResultsView({user}){
 
     const searchParams = useSelector(state => state.shortlists.parameters)
 
-    
+    const availCheck = (connection => {
+        
+        let noConflict = true
+        let i = 0
 
+        if (connection['bookings'].length === 0) return noConflict;
+
+        while(noConflict && i < connection['bookings'].length){
+            const booking = connection['bookings'][i]
+            const [ bookingStart, bookingEnd ] = booking
+            const booking_start = new Date(bookingStart)
+            const booking_end = new Date(bookingEnd)
+            const paramsStart = new Date(searchParams['start_date'])
+            
+            console.log(">>> connections being checked for avail:", connection.first_name);
+            console.log(">>>booking_start", booking_start);
+            console.log(">>> booking_end", booking_end);
+            console.log(">>>> paramsStart:", paramsStart);
+
+            if(booking_start < paramsStart && paramsStart < booking_end ) noConflict = false
+
+            
+            i++
+        }
+
+        return noConflict;
+
+
+       
+
+            
+    })
+            
+    let avail_filtered_results = []
+
+    if(searchResults.length > 0 && (searchResults !== null)){
+         avail_filtered_results = searchResults.filter(connection => availCheck(connection))
+
+    }
     
 
     const submitHandler = (e)=>{
@@ -65,12 +102,13 @@ function SearchResultsView({user}){
 
         
         {/*  Array.map of returned results tiles */}
-        {searchResults.map( result =>{
+        {avail_filtered_results.length > 0 ? avail_filtered_results.map( result =>{
             return (
             
                 <div key={result.id}>{result.first_name}</div>
             )
-        })}
+        }):
+        <p>No one matches this search.</p>}
         </>
     )
 }
