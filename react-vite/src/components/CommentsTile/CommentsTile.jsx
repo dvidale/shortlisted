6d8fe3d90ff9e15fbda9c36cc998a98048c9ddd7
DeAndre <deandre.vidale@gmail.com>
2024-08-31 16:45:12 -0700

    added db relationship from comments to shortlists

--- react-vite/src/components/CommentsTile/CommentsTile.jsx
+++ react-vite/src/components/CommentsTile/CommentsTile.jsx
@@ -10,11 +10,10 @@ import { useModal } from "../../context/Modal";
 import CommentEditorModal from "../CommentEditorModal/CommentEditorModal";
 
 function CommentsTile({ shortlist, referralIdx }) {
-
   const current_thread = useSelector(
     (state) => state.comments.comment_threads[referralIdx]
   );
- 
+
   const user = useSelector((state) => state.session.user);
 
   const { setModalContent } = useModal();
@@ -32,14 +31,13 @@ function CommentsTile({ shortlist, referralIdx }) {
     setModalContent(<DeleteCommentModal commentId={commentId} />);
   };
 
-let current_thread_sorted;
+  let current_thread_sorted;
 
-if(current_thread){
-current_thread_sorted = Object.values(current_thread).sort( comment => comment['createdAt'])
-
+  if (current_thread) {
+    current_thread_sorted = Object.values(current_thread).sort(
+      (comment) => comment["createdAt"]
+    );
   }
-
-
 
   return (
     <>
@@ -47,42 +45,51 @@ current_thread_sorted = Object.values(current_thread).sort( comment => comment['
         Object.values(current_thread_sorted).length > 0 &&
         Object.values(current_thread_sorted).map((comment) => {
           return (
-         
-              <div key={comment.id} className="comment-bubble-and-avatar">
-                <div  className="commenter_name">{comment.commenter_name !== user.first_name && <span><VscAccount />{comment.commenter_name}</span>}</div>
+            <div key={comment.id} className="comment-bubble-and-avatar">
+              <div key={`${comment.id}-name`} className="commenter_name">
+                {comment.commenter_name !== user.first_name && (
+                  <span key={`${comment.id}-avatar`}>
+                    <VscAccount />
+                    {comment.commenter_name}
+                  </span>
+                )}
+              </div>
 
-                <div className="comment-text"><span className="message-bubble">{comment.text}</span></div>
-               
-         
+              <div key={`${comment.id}-text`} className="comment-text">
+                <span key={`${comment.id}-bubble`} className="message-bubble">{comment.text}</span>
+              </div>
 
-                <div className="edit-delete-list-btns">
-                {comment.commenter_id === user.id &&
-                  <button
+              <div key={`${comment.id}-edit-delete`} className="edit-delete-list-btns">
+                {comment.commenter_id === user.id && (
+                  <button key={`${comment.id}-edit`}
                     onClick={() => commentEditor(comment.text, comment.id)}
                   >
                     Edit
-                  </button>}
-                  {comment.commenter_id === user.id &&
-                  <button onClick={() => deleteCommentModal(comment.id)}>
+                  </button>
+                )}
+                {comment.commenter_id === user.id && (
+                  <button key={`${comment.id}-delete`} onClick={() => deleteCommentModal(comment.id)}>
                     Delete
-                  </button>}
-       {comment.commenter_id === user.id && <div className="you_icon"><VscAccount />You</div>}
-
-
-                </div>
-                
+                  </button>
+                )}
+                {comment.commenter_id === user.id && (
+                  <div key={`${comment.id}-you-icon`} className="you_icon">
+                    <VscAccount />
+                    You
+                  </div>
+                )}
               </div>
-          
+            </div>
           );
         })}
 
-    <div className="comment-form-container">
-  <CommentForm
-        shortlist={shortlist}
-        referralIdx={referralIdx}
-        user={user}
-      />
-</div> 
+      <div className="comment-form-container">
+        <CommentForm
+          shortlist={shortlist}
+          referralIdx={referralIdx}
+          user={user}
+        />
+      </div>
     </>
   );
 }
