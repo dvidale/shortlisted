dada0a0f04cc0016e4412c65ed69d56d5aef0a82
DeAndre <deandre.vidale@gmail.com>
2024-09-11 22:31:15 -0700

    changed Loading indicator for search results to white

--- react-vite/src/components/SearchConnectionsForm/SearchConnectionsForm.jsx
+++ react-vite/src/components/SearchConnectionsForm/SearchConnectionsForm.jsx
@@ -195,7 +195,7 @@ function SearchConnectionsForm({setSearchSubmitted, searchFormView, setIsLoading
        <button id='submit' type="submit">SEARCH</button>
        <p className='error'>{errors.serverError}</p>
        
-       <p>{isLoading && `Loading...`}</p>
+       <h4 style={{'color': 'white'}}>{isLoading && `Loading...`}</h4>
         </form>
     
 
