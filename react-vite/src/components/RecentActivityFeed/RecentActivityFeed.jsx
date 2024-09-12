3028cd2210a8a86fa91b45b4090e22b86383fb00
DeAndre <deandre.vidale@gmail.com>
2024-09-11 20:48:41 -0700

    added recent activity center panel

---
+++ react-vite/src/components/RecentActivityFeed/RecentActivityFeed.jsx
@@ -0,0 +1,41 @@
+import './recent-activity.css'
+
+function RecentActivityFeed(){
+
+    const activities =[
+        {
+            'msg':'Brandon Carter left a comment on "NYC Editors for Indie Documentary"'
+        },
+        {
+            'msg':'Faith Davis left a comment on "LA Assistant Editors for HBO Drama"'
+        },
+        {
+            'msg':'Naina Raj shortlisted you for "Editors for 2025 Indies in LA"'
+        },
+        {
+            'msg': 'Isabella Williams shortlisted you for "AEs for Award-Winning TV Series"'
+        },
+        {
+            'msg':'Chetan Sharma joined Shortlisted with your invitation'
+        },
+        {
+            'msg':'Karla Ybarra will become available in 30 days.'
+        }
+
+
+
+    ]
+
+    return(
+        <>
+        <h1>Recent Activity</h1>
+        { activities.map(activity =>(
+
+            <div key={activity.msg}>{activity.msg}</div>
+        ))}
+        
+        </>
+    )
+}
+
+export default RecentActivityFeed
