bd47278a50ba37dd0bc387869f112537defeaf08
DeAndre <deandre.vidale@gmail.com>
2024-09-29 21:48:01 -0700

    successfully added a new booking via frontend

--- app/forms/booking_form.py
+++ app/forms/booking_form.py
@@ -5,5 +5,5 @@ from wtforms.validators import DataRequired
 class BookingForm(FlaskForm):
     user_id = IntegerField('user_id')
     shortlist_id = IntegerField('shortlist_id')
-    start_date = StringField('start_date', validators=[DataRequired(message='A start date is required')])
-    end_date = StringField('end_date',validators=[DataRequired(message='An end date is required')] )
+    # start_date = StringField('start_date', validators=[DataRequired(message='A start date is required')])
+    # end_date = StringField('end_date',validators=[DataRequired(message='An end date is required')] )
