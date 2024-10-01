442017e6089e64a161990851a123ac650c2b66ef
DeAndre <deandre.vidale@gmail.com>
2024-10-01 07:57:49 -0700

    testing logic to handle array of error messages

--- react-vite/src/components/ServerMessageModal/ServerMessageModal.jsx
+++ react-vite/src/components/ServerMessageModal/ServerMessageModal.jsx
@@ -8,11 +8,14 @@ function ServerMessageModal({message}){
 // write logic for if the message is an array of messages vs a single message
 console.log("message in server msg modal", message);
 
+
 return(
 <>
 <div className="server-message-modal">
             <h2>Server Message</h2>
-            <p>{message}</p>
+            <p>{Object.values(message).map(
+                msg => <div key={msg}>{msg}</div>
+            )}</p>
             <button className="server-message-ok-btn" type='button' onClick={()=> closeModal()}>Ok</button>
         </div>
 </>
