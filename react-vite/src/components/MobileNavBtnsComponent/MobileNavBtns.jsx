a58f7bd53e22a209918acc64b37b5e6b70365844
DeAndre <deandre.vidale@gmail.com>
2024-10-17 19:29:55 -0700

    added react-responsive library for mobile layout logic

---
+++ react-vite/src/components/MobileNavBtnsComponent/MobileNavBtns.jsx
@@ -0,0 +1,22 @@
+import "../../../src/index.css"
+
+
+
+function MobileNavBtns(){
+
+
+
+    return(
+        <>
+        
+        <div className="mbl-btns"> My Shortlists </div>
+        <div className="mbl-btns"> Profile </div>
+        <div className="mbl-btns"> My Calendar </div>
+
+        </>
+
+    )
+}
+
+
+export default MobileNavBtns
