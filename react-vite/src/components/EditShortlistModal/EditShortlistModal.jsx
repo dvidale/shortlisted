ca0f909d2518904072f0a2fa55531beb0078fb9c
DeAndre <deandre.vidale@gmail.com>
2024-11-08 16:32:18 -0800

    started shortlists mobile view

---
+++ react-vite/src/components/EditShortlistModal/EditShortlistModal.jsx
@@ -0,0 +1,26 @@
+
+
+function EditShortlistModal(){
+
+return (
+    <>
+    Editor
+    {/* <form id="edit-shortlist-modal">
+        <label>
+            Title:
+            <input id='edit-shortlist-modal-title' type='text'></input>
+        </label>
+        <label>
+            Description:
+            <input id='edit-shortlist-modal-desc' type='text'></input>
+        </label>
+    </form> */}
+    
+    </>
+)
+
+
+}
+
+
+export default EditShortlistModal
