73f119fb47eefa109042dfea3e2ee1f1b5947aa9
DeAndre <deandre.vidale@gmail.com>
2024-11-09 21:51:46 -0800

    WIP edit shortlist mobile modal

--- react-vite/src/components/EditShortlistModal/EditShortlistModal.jsx
+++ react-vite/src/components/EditShortlistModal/EditShortlistModal.jsx
@@ -1,20 +1,53 @@
+import { useDispatch } from 'react-redux'
+import { useModal } from '../../context/Modal'
+import { useNavigate } from 'react-router-dom'
+
+function EditShortlistModal({title, setTitle, description, setDescription, shortlistId}){
+
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const { closeModal } = useModal()
 
 
-function EditShortlistModal(){
+    const submitHandler = (e) =>{
+        e.preventDefault()
 
+        const err = {};
+    
+    if (title === '' || title.length === 0) err.title = "A title is required";
+
+    setErrors(err);
+
+    if (Object.keys(err).length === 0) {
+      editSwitch();
+
+      const formData = {
+        title,
+        description,
+      };
+
+      const shortlistId = shortlist.id;
+
+      dispatch(updateShortlist(shortlistId, JSON.stringify(formData)));
+      // TODO: Add server response modal
+    }
+
+
+    }
 return (
     <>
     Editor
-    {/* <form id="edit-shortlist-modal">
-        <label>
+    <form id="edit-shortlist-modal" onSubmit={submitHandler}>
+        <label htmlFor="edit-shortlist-modal-title">
             Title:
             <input id='edit-shortlist-modal-title' type='text'></input>
         </label>
-        <label>
+        <label htmlFor='edit-shortlist-modal-desc'>
             Description:
             <input id='edit-shortlist-modal-desc' type='text'></input>
         </label>
-    </form> */}
+        <button id='mobile-shortlist-update' type="submit">Update</button>
+    </form>
     
     </>
 )
