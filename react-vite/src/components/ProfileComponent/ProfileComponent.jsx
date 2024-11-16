7c37f8964651e730e3460328496352d155d7c4e9
DeAndre <deandre.vidale@gmail.com>
2024-11-15 18:00:20 -0600

    added user attributes to profile panel for mobile

--- react-vite/src/components/ProfileComponent/ProfileComponent.jsx
+++ react-vite/src/components/ProfileComponent/ProfileComponent.jsx
@@ -15,7 +15,26 @@ function ProfileComponent({user}){
     return (
         <div id='profile-container'>
         <div className={'profile-img-container'} style={bannerImgStyle}> </div>
+        <h1 id="user-name">
         {user.first_name} {user.last_name}
+        </h1>
+        
+        <div id="user-job-titles">
+            {user.job_titles.join(" • ")}
+        </div>
+        <h3 id="industry-area-title" className='profile-heading'>Industry Area:</h3>
+        <div>
+            {user.industry_areas.join(" • ")}
+        </div>
+        <h3 id="genre-title" className='profile-heading'>Genre:</h3>
+        <div>
+            {user.genres.join(" • ")}
+        </div>
+        <h3 id="location-title" className='profile-heading'>Location:</h3>
+        <div>
+            {user.locations.join(" • ")}
+        </div>
+
         
         </div>
     )
