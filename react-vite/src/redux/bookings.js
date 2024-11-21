71d520001877bb9a4da9a4b5776602a8ebeb24bd
DeAndre <deandre.vidale@gmail.com>
2024-11-21 14:08:46 -0800

    DELETE booking functionality complete

--- react-vite/src/redux/bookings.js
+++ react-vite/src/redux/bookings.js
@@ -1,6 +1,6 @@
 const GET_MY_BOOKINGS = '/bookings/GET_MY_BOOKINGS'
 const CREATE_A_BOOKING = '/bookings/CREATE_A_BOOKING'
-
+const DELETE_A_BOOKING = '/bookings/DELETE_A_BOOKING'
 
 export const getAllMyBookings = (data) =>{
 
@@ -18,6 +18,13 @@ export const createABooking = (data) => {
     }
 }
 
+export const deleteABooking = (id) => {
+
+    return {
+        type: DELETE_A_BOOKING,
+        payload: id
+    }
+}
 
 
 /*---------------------------
@@ -71,6 +78,32 @@ export const createBooking = (bookingData) => async (dispatch) => {
     }
 }
 
+export const deleteBooking = (id) => async(dispatch) =>{
+
+    const url = `/api/bookings/${id}`
+    const method = 'DELETE'
+
+    const options = {method}
+
+    const response = await fetch(url, options);
+
+    if(response.ok){
+
+        const data = await response.json()
+        dispatch(deleteABooking(data['id']))
+
+    }else{
+
+        const serverError = await response.json()
+        console.log("serverError from new Booking", serverError);
+        return serverError;
+    }
+
+
+
+
+}
+
 /*--------------------------
          REDUCER
 ------------------------------*/
@@ -92,6 +125,11 @@ const bookingsReducer = (state = intitalState, action) =>{
             const newBooking = action.payload;
             newState.user_bookings[newBooking.id] = newBooking;
             return newState;
+        }
+        case DELETE_A_BOOKING:{
+            const newState = {...state}
+            delete newState.user_bookings[action.payload]
+            return newState
         }
         default:
             return state;
