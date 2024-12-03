738a8c2774374cd34bd7357c4a06424732d518e5
DeAndre <deandre.vidale@gmail.com>
2024-12-02 17:37:06 -0800

    WIP styling referral thread

--- react-vite/src/components/ReferralShortlistView/ReferralShortlistView.jsx
+++ react-vite/src/components/ReferralShortlistView/ReferralShortlistView.jsx
@@ -1,20 +1,29 @@
+import ReferralShortlistMsgs from "../ReferralShortlistMsgs/ReferralShortlistMsgs"
 import "../SingleShortlistView/single-shortlist.css"
 import { useSelector } from "react-redux"
 
 
 function ReferralShortlistView({referralListIdx}){
 
-    let shortlist = useSelector( state => state.referrals.referral_details[referralListIdx])
+    let referral = useSelector( state => state.referrals.referral_details[referralListIdx])
    
 
 return (
 
     <>
-    <div>Referral convo here</div>
-    { shortlist && 
+    
+    { referral && 
     
 <>    
-    <div>{shortlist.shortlist_title}</div>
+   
+    <div>{referral.shortlist_title}</div>
+    <div>{referral.shortlist_desc}</div>
+    
+     <div><img className="user-avatar" src={referral.createdby_photo}/></div>
+    <div>{referral.createdby_fname} {referral.createdby_lname}</div>
+    <div className={'comment-thread'}>
+        <ReferralShortlistMsgs referral={referral}/>
+    </div>
     
   </>  
 }
