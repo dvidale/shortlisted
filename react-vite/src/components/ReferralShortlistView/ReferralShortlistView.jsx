8c86f85d32809a4d644040fefd9655dac6864d74
DeAndre <deandre.vidale@gmail.com>
2024-12-25 16:38:38 -0800

    WIP building messages back button for referral single shortlist mobile view

--- react-vite/src/components/ReferralShortlistView/ReferralShortlistView.jsx
+++ react-vite/src/components/ReferralShortlistView/ReferralShortlistView.jsx
@@ -1,9 +1,11 @@
 import ReferralShortlistMsgs from "../ReferralShortlistMsgs/ReferralShortlistMsgs"
 import "../SingleShortlistView/single-shortlist.css"
 import { useSelector } from "react-redux"
-
+import { useMediaQuery } from 'react-responsive'
 
 function ReferralShortlistView({referralListIdx}){
+
+    const isTabletOrMobile = useMediaQuery({query: '(max-width: 1100px)'})
 
     let referral = useSelector( state => state.referrals.referral_details[referralListIdx])
    
@@ -15,7 +17,9 @@ return (
     { referral && 
     
 <>    
-   
+   { isTabletOrMobile &&  <div id="messages-back-btn">
+    {`<`} Messages
+   </div>}
     <div>{referral.createdby_fname} {referral.createdby_lname}{`'s`} Shortlist:</div>
     <div> <h2 className="panel-heading">{referral.shortlist_title}</h2> </div>
 
