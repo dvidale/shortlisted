505e3fee320ac30efa437ff1c69cd604444dfb1e
DeAndre <deandre.vidale@gmail.com>
2025-01-03 15:50:17 -0800

    WIP adding hasMessages attribute to User model

--- app/models/user.py
+++ app/models/user.py
@@ -26,6 +26,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(40), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
     profile_img_url = db.Column(db.String(255))
+    hasMessages = db.Column(db.Boolean)
     createdAt = db.Column(db.DateTime, nullable=False, default=db.func.now())
     updatedAt = db.Column(db.DateTime, nullable=False, default=db.func.now(), onupdate=db.func.now())
 
@@ -89,7 +90,8 @@ class User(db.Model, UserMixin):
             'genres':[ name.genre_name for name in self.genres ],
             'locations': [name.city for name in self.locations],
             'bookings': [(booking.start_date, booking.end_date) for booking in self.bookings],
-            'profile_img_url': self.profile_img_url
+            'profile_img_url': self.profile_img_url,
+            'hasMessages': self.hasMessages
 
         }
     
