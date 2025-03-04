99989ff38204a84959193cf92274dd7b95d53908
DeAndre <deandre.vidale@gmail.com>
2025-03-04 05:37:14 -0800

    added intentional 404 message for no messages received in GET comments endpoint

--- react-vite/src/components/HomeView/HomeView.jsx
+++ react-vite/src/components/HomeView/HomeView.jsx
@@ -1,5 +1,5 @@
 import "../../../src/index.css";
-import { useMediaQuery } from 'react-responsive'
+import { useMediaQuery } from "react-responsive";
 import SearchConnectionsForm from "../SearchConnectionsForm/SearchConnectionsForm";
 import SearchResultsView from "../SearchResultsView/SearchResultsView";
 import { useSelector, useDispatch } from "react-redux";
@@ -10,12 +10,10 @@ import { FaMinusCircle } from "react-icons/fa";
 import SplashPageComponent from "../SplashPageComponent/SplashPageComponent";
 import MyShortlists from "../MyShortlists/MyShortlists";
 import MyListings_Calendar from "../MyListings_Calendar/MyListings_Calendar";
-import { getCommentThreads, 
-  getReceivedMsgsCount 
-} from "../../redux/comments";
+import { getCommentThreads, getReceivedMsgsCount } from "../../redux/comments";
 import SingleShortlistView from "../SingleShortlistView/SingleShortlistView";
 import RecentActivityFeed from "../RecentActivityFeed/RecentActivityFeed";
-import MobileNavBtns from "../MobileNavBtnsComponent/MobileNavBtns"
+import MobileNavBtns from "../MobileNavBtnsComponent/MobileNavBtns";
 import BookingsPanel from "../MyListings_Calendar/BookingsPanel";
 import ProfileComponent from "../ProfileComponent/ProfileComponent";
 import ReferralShortlistView from "../ReferralShortlistView/ReferralShortlistView";
@@ -23,111 +21,64 @@ import { PanelViews } from "../../context/PanelView";
 import MyListingsPanel from "../MyListings_Calendar/MyListingsPanel";
 import { getReferrals } from "../../redux/my-referrals";
 
-
-
-
-
 // TODO: Write all labels to match their element ids
 function HomeView() {
-
-
-  const isTabletOrMobile = useMediaQuery({query: '(max-width: 1100px)'})
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1100px)" });
   const dispatch = useDispatch();
 
   const user = useSelector((state) => state.session.user);
 
-  const { centerPanel, setCenterPanel, leftPanel, setLeftPanel } = useContext(PanelViews)
+  const { centerPanel, setCenterPanel, leftPanel, setLeftPanel } =
+    useContext(PanelViews);
 
   const saved_shortlists = useSelector((state) => state.shortlists.saved_lists);
-  const user_bookings = useSelector(state => state.bookings.user_bookings)
-  
+  const user_bookings = useSelector((state) => state.bookings.user_bookings);
+
   const shortlists_state = useSelector((state) => state.shortlists);
 
   let newestIdx;
-if(saved_shortlists && Object.keys(saved_shortlists).length > 0){
-newestIdx = Object.keys(saved_shortlists).reverse()[0]
-}
-
-const [shortlistIdx, setShortlistIdx] = useState(newestIdx || null);
-const [editForm, setEditForm] = useState(false);
-const [toggleSymbol, setToggleSymbol] = useState(`+`);
-const [referralListIdx, setReferralListIdx ] = useState(null)
-
-
-
-const [isLoading, setIsLoading] = useState(false)
-
-
-
-const toggleFormView = () => {
-  setToggleSymbol(!toggleSymbol);
-  
-  if(leftPanel === 'my-shortlists'){
-    setLeftPanel('shortlist-search')
-  }else{
-    setLeftPanel('my-shortlists')
+  if (saved_shortlists && Object.keys(saved_shortlists).length > 0) {
+    newestIdx = Object.keys(saved_shortlists).reverse()[0];
   }
-  
-  setCenterPanel('recent-activity')
-  
-};
 
-const currentCommentThreads = useSelector(state => state.comments.comment_threads)
+  const [shortlistIdx, setShortlistIdx] = useState(newestIdx || null);
+  const [editForm, setEditForm] = useState(false);
+  const [toggleSymbol, setToggleSymbol] = useState(`+`);
+  const [referralListIdx, setReferralListIdx] = useState(null);
 
+  const [isLoading, setIsLoading] = useState(false);
 
-// Periodically check for new messages for shortlist creators and referrals every 10 seconds
+  const toggleFormView = () => {
+    setToggleSymbol(!toggleSymbol);
 
-useEffect(()=>{
-  if(user){
-// as a referral, check if new messages have been received from a shortlist creator
-    setInterval(() => {
+    if (leftPanel === "my-shortlists") {
+      setLeftPanel("shortlist-search");
+    } else {
+      setLeftPanel("my-shortlists");
+    }
 
-      dispatch(getReceivedMsgsCount(user.id)).then( (receivedMsgsCount )=> {
-        
-        if (receivedMsgsCount > 0){
-          dispatch(getReferrals(user.id))
-          // console.log(">>> last messages check", new Date())
-        }
-      })
+    setCenterPanel("recent-activity");
+  };
 
-    }, 10*1000);
-  }
-}, [user, dispatch ])
+  // Periodically check for new messages for shortlist creators and referrals every 10 seconds
 
+  useEffect(() => {
+    if (user) {
+      // as a referral, check if new messages have been received from a shortlist creator
+      const checkReferralMsgs = setInterval(() => {
+        dispatch(getReceivedMsgsCount(user.id)).then((receivedMsgsCount) => {
+          if (receivedMsgsCount > 0) {
+            dispatch(getReferrals(user.id));
+            // console.log(">>> last messages check", new Date())
+          }
+        });
+      }, 10 * 1000);
 
+      return () => clearInterval(checkReferralMsgs);
+    }
+  }, [user, dispatch]);
 
-// as a shortlist creator, check if new messages have been received from a referral
-
-useEffect(()=>{
-
-if(user){
-
-  dispatch(getCommentThreads(user.id))
-
-}
-
-
-}, [dispatch, user])
-
-
-useEffect(()=>{
- 
-  const threadHasComments = Object.keys(currentCommentThreads).length > 0
-  
-  if(user && threadHasComments){
-
-    setInterval(() => {
-      
-      dispatch(getCommentThreads(user.id))
-      
-    }, 10*1000);
-  }
-},[user, dispatch, currentCommentThreads])
-
-
-
-  useEffect(()=>{},[shortlists_state])
-  
+  useEffect(() => {}, [shortlists_state]);
 
   useEffect(() => {
     if (user) {
@@ -138,42 +89,48 @@ useEffect(()=>{
   useEffect(() => {
     if (saved_shortlists && user) {
       dispatch(getCommentThreads(user.id));
-      setShortlistIdx(newestIdx)
-
+      setShortlistIdx(newestIdx);
     }
-
-    
-
   }, [saved_shortlists, dispatch, user, newestIdx]);
 
+  // as a shortlist creator, check if new messages have been received from a referral
 
- 
+  const currentCommentThreads = useSelector(
+    (state) => state.comments.comment_threads
+  );
 
+  useEffect(() => {
+    const threadHasComments = Object.keys(currentCommentThreads).length > 0;
 
+    if (user && threadHasComments) {
+      const checkShortlistComments = setInterval(() => {
+        dispatch(getCommentThreads(user.id));
+      }, 10 * 1000);
 
+      return () => clearInterval(checkShortlistComments);
+    }
+  }, [user, dispatch, currentCommentThreads]);
 
   return (
     <>
       {user ? (
         <>
-          <div className='mobile-title-logo'>Shortlisted</div>
+          <div className="mobile-title-logo">Shortlisted</div>
           <div id="search-and-my-shortlist-container">
-  
             <button id="new-shortlist-btn" onClick={() => toggleFormView()}>
-              <h1>
-            
-                New Shortlist 
-                </h1>
-                <h1>
-                {toggleSymbol ? <FaPlusCircle /> : <FaMinusCircle />}
-             
-              </h1>
+              <h1>New Shortlist</h1>
+              <h1>{toggleSymbol ? <FaPlusCircle /> : <FaMinusCircle />}</h1>
             </button>
-            
 
             <div
               id="build-shortlist-form"
-              style={{display: leftPanel === 'shortlist-search' || centerPanel === 'shortlist-search' ? 'flex' : 'none'}}
+              style={{
+                display:
+                  leftPanel === "shortlist-search" ||
+                  centerPanel === "shortlist-search"
+                    ? "flex"
+                    : "none",
+              }}
             >
               <SearchConnectionsForm
                 setIsLoading={setIsLoading}
@@ -182,98 +139,109 @@ useEffect(()=>{
                 setToggleSymbol={setToggleSymbol}
               />
             </div>
-           
-         
-        {!isTabletOrMobile && 
-          <div
-          id="my-shortlists"
-          className={'left-panel'}
-          style={{display: leftPanel === 'my-shortlists' ? 'flex' : 'none'}}
-          
-          >
-              <MyShortlists
-                shortlistIdx={shortlistIdx}
-                setShortlistIdx={setShortlistIdx}
-                saved_shortlists={saved_shortlists}
-                setEditForm={setEditForm}
-                />
-            </div>
-            }
-              
 
+            {!isTabletOrMobile && (
+              <div
+                id="my-shortlists"
+                className={"left-panel"}
+                style={{
+                  display: leftPanel === "my-shortlists" ? "flex" : "none",
+                }}
+              >
+                <MyShortlists
+                  shortlistIdx={shortlistIdx}
+                  setShortlistIdx={setShortlistIdx}
+                  saved_shortlists={saved_shortlists}
+                  setEditForm={setEditForm}
+                />
+              </div>
+            )}
           </div>
           <div
             id="single-shortlist-view"
             className="center-panel"
-            style={{display: centerPanel === 'single-shortlist' ? 'flex' : 'none'}}
+            style={{
+              display: centerPanel === "single-shortlist" ? "flex" : "none",
+            }}
           >
             <SingleShortlistView
               setEditForm={setEditForm}
               editForm={editForm}
               setShortlistIdx={setShortlistIdx}
               shortlistIdx={shortlistIdx}
-              
             />
           </div>
 
           <div
             id="search-results-view"
-            className='center-panel'
-            style={{display: centerPanel === 'search-results' ? 'flex' : 'none'}}
+            className="center-panel"
+            style={{
+              display: centerPanel === "search-results" ? "flex" : "none",
+            }}
           >
             <SearchResultsView
               user={user}
               toggleSymbol={toggleSymbol}
-              setToggleSymbol={setToggleSymbol} 
+              setToggleSymbol={setToggleSymbol}
             />
           </div>
 
-
-            <div id="recent-activity-view"
-            className={'center-panel'}
-            style={{display: centerPanel === 'recent-activity' ? 'flex' : 'none'}}
-            >
-              <RecentActivityFeed/>
-            </div>
-
-           <div id="referral-shortlist-view"
-           className={"center-panel"}
-           style={{display: centerPanel === 'referral-shortlist' ? 'flex' : 'none'}}>
-            <ReferralShortlistView referralListIdx={referralListIdx}/>
-            </div> 
-
-            <div id='referral-listings-view'
-            className={'center-panel'}
-            style={{display: centerPanel === 'my-listings' ? 'flex' : 'none'}}>
-              <MyListingsPanel setReferralListIdx={setReferralListIdx}/>
-            </div>
-         
-            {isTabletOrMobile && centerPanel === 'calendar' &&
-          <div>
-            <BookingsPanel user_bookings={user_bookings}/>
+          <div
+            id="recent-activity-view"
+            className={"center-panel"}
+            style={{
+              display: centerPanel === "recent-activity" ? "flex" : "none",
+            }}
+          >
+            <RecentActivityFeed />
           </div>
-          }
 
+          <div
+            id="referral-shortlist-view"
+            className={"center-panel"}
+            style={{
+              display: centerPanel === "referral-shortlist" ? "flex" : "none",
+            }}
+          >
+            <ReferralShortlistView referralListIdx={referralListIdx} />
+          </div>
 
-          <div id='user-profile'
-          className={"center-panel"}
-          style={{display: centerPanel === 'user-profile' ? 'flex' : 'none' }}>
-            <ProfileComponent user={user}/></div>
+          <div
+            id="referral-listings-view"
+            className={"center-panel"}
+            style={{ display: centerPanel === "my-listings" ? "flex" : "none" }}
+          >
+            <MyListingsPanel setReferralListIdx={setReferralListIdx} />
+          </div>
+
+          {isTabletOrMobile && centerPanel === "calendar" && (
+            <div>
+              <BookingsPanel user_bookings={user_bookings} />
+            </div>
+          )}
+
+          <div
+            id="user-profile"
+            className={"center-panel"}
+            style={{
+              display: centerPanel === "user-profile" ? "flex" : "none",
+            }}
+          >
+            <ProfileComponent user={user} />
+          </div>
 
           <div id="my-listings-calendar-placeholder" className="right-panel">
             <MyListings_Calendar setReferralListIdx={setReferralListIdx} />
           </div>
-          
-          {isTabletOrMobile && <div id="mobile-nav" className="mobile-nav-container">
-            <MobileNavBtns/>
-          </div>}
 
-           
-
-  </>
+          {isTabletOrMobile && (
+            <div id="mobile-nav" className="mobile-nav-container">
+              <MobileNavBtns />
+            </div>
+          )}
+        </>
       ) : (
-   <SplashPageComponent/>
-       
+        <SplashPageComponent />
       )}
     </>
   );
--- react-vite/src/redux/comments.js
+++ react-vite/src/redux/comments.js
@@ -1,233 +1,217 @@
-const GET_ALL_COMMENT_THREADS = '/comments/GET_ALL_COMMENT_THREADS'
-const ADD_A_COMMENT = '/comments/ADD_A_COMMENT'
-const EDIT_A_COMMENT = '/comments/EDIT_A_COMMENT'
-const DELETE_A_COMMENT = '/comments/DELETE_A_COMMENT'
-const CLEAR_DELETED_THREAD = '/comments/CLEAR_DELETED_THREAD'
-const RESET_COMMENTS = '/comments/RESET_COMMENTS'
-const UPDATE_RECEIVED_MSGS_COUNT = '/comments_UPDATE_RECEIVED_MSGS_COUNT'
+const GET_ALL_COMMENT_THREADS = "/comments/GET_ALL_COMMENT_THREADS";
+const ADD_A_COMMENT = "/comments/ADD_A_COMMENT";
+const EDIT_A_COMMENT = "/comments/EDIT_A_COMMENT";
+const DELETE_A_COMMENT = "/comments/DELETE_A_COMMENT";
+const CLEAR_DELETED_THREAD = "/comments/CLEAR_DELETED_THREAD";
+const RESET_COMMENTS = "/comments/RESET_COMMENTS";
+const UPDATE_RECEIVED_MSGS_COUNT = "/comments_UPDATE_RECEIVED_MSGS_COUNT";
 
-export const getAllCommentThreads = (data)=>{
-    return {
-        type: GET_ALL_COMMENT_THREADS,
-        payload:data
-    }
-}
+export const getAllCommentThreads = (data) => {
+  return {
+    type: GET_ALL_COMMENT_THREADS,
+    payload: data,
+  };
+};
 
-export const updateReceivedMsgsCount = (data) =>{
+export const updateReceivedMsgsCount = (data) => {
+  return {
+    type: UPDATE_RECEIVED_MSGS_COUNT,
+    payload: data,
+  };
+};
 
-    return{
-        type: UPDATE_RECEIVED_MSGS_COUNT,
-        payload: data
-    }
-}
+export const addAComment = (data) => {
+  return {
+    type: ADD_A_COMMENT,
+    payload: data,
+  };
+};
 
-export const addAComment = (data) =>{
-    return {
-        type: ADD_A_COMMENT,
-        payload: data
-    }
-}
+export const editAComment = (data) => {
+  return {
+    type: EDIT_A_COMMENT,
+    payload: data,
+  };
+};
 
-export const editAComment = (data) =>{
-    return {
-        type: EDIT_A_COMMENT,
-        payload: data
-    }
-}
+export const deleteAComment = (id, thread) => {
+  return {
+    type: DELETE_A_COMMENT,
+    payload: [id, thread],
+  };
+};
 
-export const deleteAComment = (id, thread) =>{
-    return {
-        type: DELETE_A_COMMENT,
-        payload: [id, thread]
-    }
-}
-
-export const clearDeletedThreads = (data) =>{
-    return {
-        type: CLEAR_DELETED_THREAD,
-        payload: data
-    }
-}
+export const clearDeletedThreads = (data) => {
+  return {
+    type: CLEAR_DELETED_THREAD,
+    payload: data,
+  };
+};
 
 export const resetComments = () => {
-    return {
-        type: RESET_COMMENTS,
-        payload: null
-    }
-}
+  return {
+    type: RESET_COMMENTS,
+    payload: null,
+  };
+};
 
 /*-------
 THUNKS
 -------*/
 
 export const getReceivedMsgsCount = (id) => async (dispatch) => {
+  const url = `/api/comments/receivedmsgs/${id}`;
 
-const url = `/api/comments/receivedmsgs/${id}`
+  const response = await fetch(url);
 
-const response = await fetch(url)
-
-if(response.ok){
-
-    const data = await response.json()
-    dispatch(updateReceivedMsgsCount(data.receivedMsgsCount))
+  if (response.ok) {
+    const data = await response.json();
+    dispatch(updateReceivedMsgsCount(data.receivedMsgsCount));
 
     return data.receivedMsgsCount;
-}
-
-}
-
+  }
+};
 
 export const getCommentThreads = (id) => async (dispatch) => {
+  const url = `/api/comments/${id}`;
 
-const url = `/api/comments/${id}`
+  const response = await fetch(url);
 
-const response = await fetch(url)
+  if (response.ok) {
+    const data = await response.json();
+    // TODO - try adding a conditional to only run the dispatch if the data value changed since the last fetch
 
-if(response.ok){
+    dispatch(getAllCommentThreads(data));
 
-    const data = await response.json()
-    
-    dispatch(getAllCommentThreads(data))
-    
-    return data
+    return data;
+  } else if (response.status === 404) {
+    const noMessages = await response.json();
 
-}
-}
+    return noMessages;
+  } else {
+    const serverError = await response.json();
 
-export const addComment = (commentData) => async (dispatch) =>{
+    return serverError;
+  }
+};
 
-    const url = `/api/comments/new`
-    const method = 'POST'
-    const headers = {'Content-Type' : 'application/json'}
-    const body = commentData
-    const options = {method, headers, body}
-    
+export const addComment = (commentData) => async (dispatch) => {
+  const url = `/api/comments/new`;
+  const method = "POST";
+  const headers = { "Content-Type": "application/json" };
+  const body = commentData;
+  const options = { method, headers, body };
 
-    const response = await fetch(url, options)
+  const response = await fetch(url, options);
 
-    if(response.ok){
+  if (response.ok) {
+    const data = await response.json();
+    dispatch(addAComment(data));
 
-        const data = await response.json()
-        dispatch(addAComment(data))
-
-        // return data
-    }else{
-        const serverError = await response.json()
-        return serverError 
-    }
-}
+    // return data
+  } else {
+    const serverError = await response.json();
+    return serverError;
+  }
+};
 
 export const editComment = (id, commentData) => async (dispatch) => {
+  const url = `/api/comments/${id}`;
+  const method = "PUT";
+  const headers = { "Content-Type": "application/json" };
+  const body = commentData;
+  const options = { method, headers, body };
 
-    const url = `/api/comments/${id}`
-    const method = 'PUT'
-    const headers = {'Content-Type' : 'application/json'}
-    const body = commentData
-    const options = {method, headers, body}
-    
-    const response = await fetch(url, options)
+  const response = await fetch(url, options);
 
-    if(response.ok){
+  if (response.ok) {
+    const data = await response.json();
+    dispatch(editAComment(data));
 
-        const data = await response.json()
-        dispatch(editAComment(data))
+    return data;
+  }
+};
 
-        return data
-    }
+export const deleteComment = (id) => async (dispatch) => {
+  const url = `/api/comments/${id}`;
+  const method = "DELETE";
+  const options = { method };
 
+  const response = await fetch(url, options);
 
-}
+  if (response.ok) {
+    const data = await response.json();
+    dispatch(deleteAComment(id, data["target_thread"]));
 
-export const deleteComment = (id) => async (dispatch) =>{
+    return { message: "deleted successfully" };
+  }
+};
 
-const url = `/api/comments/${id}`
-const method = 'DELETE'
-const options = {method}
-
-const response = await fetch(url, options)
-
-if(response.ok){
-
-    const data = await response.json()
-    dispatch(deleteAComment(id, data['target_thread']))
-
-    return {"message" : "deleted successfully"}
-}
-
-}
-
-export const resetCommentsState = () => async (dispatch) =>{
-
-dispatch(resetComments())
-
-
-
-}
+export const resetCommentsState = () => async (dispatch) => {
+  dispatch(resetComments());
+};
 /*-----------------
         REDUCER
 -------------------*/
 
-const initialState = { receivedMsgs: 0, comment_threads: {} }
+const initialState = { receivedMsgs: 0, comment_threads: {} };
 
-const commentsReducer = (state = initialState, action ) =>{
-    switch(action.type){
-        case UPDATE_RECEIVED_MSGS_COUNT:{
-            const newState = {...state}
-            newState.receivedMsgs = action.payload
+const commentsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case UPDATE_RECEIVED_MSGS_COUNT: {
+      const newState = { ...state };
+      newState.receivedMsgs = action.payload;
 
-            return newState
-        }
-
-        case GET_ALL_COMMENT_THREADS:{
-            const newState = {...state}
-            action.payload.forEach(thread =>{
-                const referral_id = thread[0]['referral_id']
-                newState.comment_threads[referral_id] = {}
-
-                thread.forEach(comment => {
-
-                    newState.comment_threads[referral_id][comment['id']] = comment
-                })
-
-
-            })
-            return newState
-        }
-        case ADD_A_COMMENT:{
-            const newState = {...state}
-            newState.comment_threads[action.payload['referral_id']] = action.payload['id'] = action.payload
-            return newState
-        }
-        case EDIT_A_COMMENT:{
-            const newState = {...state}
-            const target_thread = newState.comment_threads[action.payload['referral_id']]
-             target_thread[action.payload['id']] = action.payload
-            return newState    
-        }
-        case DELETE_A_COMMENT:{
-            const newState = {...state}
-            const thread_id = action.payload[1]
-            const comment_id = action.payload[0]
-            delete newState.comment_threads[thread_id][comment_id]
-            return newState
-        }
-        case CLEAR_DELETED_THREAD:{
-            const newState = { ...state}
-            action.payload.forEach( threadId => 
-                delete newState.comment_threads[threadId]
-            )
-            return newState
-        }
-        case RESET_COMMENTS:{
-            const newState = {...state,  comment_threads: {} }
-            return newState
-        }
-        default:
-            return state
-
+      return newState;
     }
 
-}
+    case GET_ALL_COMMENT_THREADS: {
+      const newState = { ...state };
+      action.payload.forEach((thread) => {
+        const referral_id = thread[0]["referral_id"];
+        newState.comment_threads[referral_id] = {};
 
+        thread.forEach((comment) => {
+          newState.comment_threads[referral_id][comment["id"]] = comment;
+        });
+      });
+      return newState;
+    }
+    case ADD_A_COMMENT: {
+      const newState = { ...state };
+      newState.comment_threads[action.payload["referral_id"]] = action.payload[
+        "id"
+      ] = action.payload;
+      return newState;
+    }
+    case EDIT_A_COMMENT: {
+      const newState = { ...state };
+      const target_thread =
+        newState.comment_threads[action.payload["referral_id"]];
+      target_thread[action.payload["id"]] = action.payload;
+      return newState;
+    }
+    case DELETE_A_COMMENT: {
+      const newState = { ...state };
+      const thread_id = action.payload[1];
+      const comment_id = action.payload[0];
+      delete newState.comment_threads[thread_id][comment_id];
+      return newState;
+    }
+    case CLEAR_DELETED_THREAD: {
+      const newState = { ...state };
+      action.payload.forEach(
+        (threadId) => delete newState.comment_threads[threadId]
+      );
+      return newState;
+    }
+    case RESET_COMMENTS: {
+      const newState = { ...state, comment_threads: {} };
+      return newState;
+    }
+    default:
+      return state;
+  }
+};
 
 export default commentsReducer;
