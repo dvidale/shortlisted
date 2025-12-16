514ebeacd6f55440f252ddc1c165b02708981649
DeAndre <deandre.vidale@gmail.com>
2025-12-15 21:14:58 -0800

    added profile images to referrals within saved single shortlist view

--- app/models/shortlist.py
+++ app/models/shortlist.py
@@ -95,7 +95,7 @@ class Shortlist(db.Model):
             "location": db.session.scalars(
                 db.select(Location.city).where(Location.id == self.location_id)
             ).first(),
-            'referral_name':[(user.first_name, user.last_name) for user in self.shortlist_referrals],
+            'referral_names':[(user.first_name, user.last_name) for user in self.shortlist_referrals],
             'referral_idxs':db.session.scalars(
                 db.select(Referral.id).where(Referral.shortlist_id == self.id)
             ).all(),
